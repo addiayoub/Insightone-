8d53613eb314aa8aba275e1a4720dabd399df707
cuentaoff <cuentaoff25@gmail.com>
2024-03-19 10:26:48 +0000

    fix import Analyse chartiste & ApiLogController

--- backend/routes/apiLogRoutes.js
+++ backend/routes/apiLogRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const apiLogController = require("../controllers/ApiLogController");
+const apiLogController = require("../controllers/apiLogController");
 const auth = require("../middlewares/auth");
 router.use(auth);
 
